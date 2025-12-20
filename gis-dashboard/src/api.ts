30d03fcbc69806fc4da6790a89c3ded710e6b283
Adhwa Hartoyo <adhwamoyafi.h003@gmail.com>
2025-12-20 17:07:36 +0700

    Tambahin https ke backend

--- gis-dashboard/src/api.ts
+++ gis-dashboard/src/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://cgsoc40c0cw4ssco4owogc8w.148.230.99.137.sslip.io/';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://api.simitigasi.id';
 console.log('API_URL:', API_URL);
